3f8d9d2c26a9d54028ca99bef6e88363799e4f83
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-04 13:01:34 +0600

    Add files via upload

---
+++ Wheile_loop.js
@@ -0,0 +1,14 @@
+
+const number = 7;
+if (number < 1){
+    console.log('choto ekya number paisi');
+}
+console.log('something new is cooking');
+
+
+
+const num = 0;
+while(num<5){
+    console.log('loooping', num);
+    num = num + 1; 
+}
