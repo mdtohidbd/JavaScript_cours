d805a995bec1e1d887612eae004e9496859d8f4d
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-04 12:55:14 +0600

    Add files via upload

---
+++ while_loop_plm.js
@@ -0,0 +1,23 @@
+ // let num = 1;
+  //while (num <10){ //focus on the condition
+     //console.log(num);
+  //  num++;
+ // }
+
+/*
+let num = 50;
+while(num <100){
+  console.log(num);
+  num++;
+}
+
+*/
+
+let num = 1;
+let sum = 0;
+while(num <=10){
+  console.log(num);
+  sum = sum + sum;
+  console.log('sum',sum);
+  num++;
+}
