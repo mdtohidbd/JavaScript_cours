c2e43b5c38f248f52d22d141a4db212aab57fc62
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-05 23:20:12 +0600

    Add files via upload

---
+++ String.js
@@ -0,0 +1,12 @@
+const school = 'rsj uk the man';
+// console.log(school);
+const subject = 'Bangla';
+const book = 'bangla';
+
+if (subject==book){
+    console.log('I am reading book aibar porikkhai fatai felmu')
+}
+
+else{
+    console.log('vat khawrw jono bosa thaki');
+}
